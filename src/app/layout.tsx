4a439158ddcdcd14b1bf8efb81d66ba0beaff3c6
ConfvsioDev <confvsio@gmail.com>
2025-01-15 12:20:14 +0100

    Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,22 @@
-'use client';
-
-import './globals.css';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect, useState } from 'react';
 import Sidebar from '@/components/Sidebar';
-import { Session } from '@supabase/supabase-js';
+import './globals.css';
 
 export const metadata = {
   title: "Gestion Swaft",
   description: "Outils de Gestion de Swaft",
 };
 
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [session, setSession] = useState<Session | null>(null);
-  const [loading, setLoading] = useState(true);
-  const supabase = createClientComponentClient();
-
-  useEffect(() => {
-    const getSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
-      setSession(session);
-      setLoading(false);
-    };
-
-    getSession();
-
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-      setLoading(false);
-    });
-
-    return () => subscription.unsubscribe();
-  }, [supabase.auth]);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen bg-zinc-900">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-indigo-500"></div>
-      </div>
-    );
-  }
-
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body className="bg-zinc-900 text-gray-200">
-        <div className="flex h-screen overflow-hidden">
-          {session && <Sidebar />}
-          <main className={`flex-1 overflow-y-auto ${session ? 'ml-64' : ''}`}>
+      <body className="flex h-screen bg-zinc-900 text-gray-200 overflow-hidden">
+        <Sidebar />
+        <div className="flex-grow flex flex-col">
+          <main className="flex-grow">
             {children}
           </main>
         </div>
@@ -59,3 +24,4 @@ export default function RootLayout({
     </html>
   );
 }
+
