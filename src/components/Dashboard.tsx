af152a9c28647388391f5b52e4f06e7e7013160e
ConfvsioDev <confvsio@gmail.com>
2025-01-15 12:47:21 +0100

    Padding

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -77,7 +77,7 @@ export default function Dashboard() {
       <div className="flex justify-center items-center h-screen bg-zinc-900">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-indigo-500"></div>
       </div>
-    );
+    )
   }
 
   if (!user) return null;
