d3ac5fa941b96530d3864e8c4e1ccdcd5e91388d
ConfvsioDev <confvsio@gmail.com>
2025-01-15 12:31:40 +0100

    Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-'use client';
+// src/app/layout.tsx
 
 import './globals.css';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect, useState } from 'react';
-import Sidebar from '@/components/Sidebar';
-import { Session } from '@supabase/supabase-js';
+import ClientLayout from './ClientLayout';
 
 export const metadata = {
   title: "Gestion Swaft",
@@ -16,35 +13,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [session, setSession] = useState<Session | null>(null);
-  const supabase = createClientComponentClient();
-
-  useEffect(() => {
-    const getSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
-      setSession(session);
-    };
-
-    getSession();
-
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, [supabase.auth]);
-
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className="bg-zinc-900 text-gray-200">
-        <div className="flex h-screen overflow-hidden">
-          {session && <Sidebar />}
-          <main className={`flex-1 overflow-y-auto ${session ? 'ml-64' : ''}`}>
-            {children}
-          </main>
-        </div>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
 }
-
